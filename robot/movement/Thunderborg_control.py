5c3f76f6ed764e8e868489c6033ac7062083ae30
tc245 <34371224+tc245@users.noreply.github.com>
2020-01-26 22:45:02 +0000

    Update Thunderborg_control.py

--- robot/movement/Thunderborg_control.py
+++ robot/movement/Thunderborg_control.py
@@ -2,6 +2,8 @@
 
 import sys
 import time
+import pantilthat
+import pygame
 
 sys.path.append('/home/pi/thunderborg')
 import ThunderBorg
@@ -10,6 +12,7 @@ TB = ThunderBorg.ThunderBorg()
 TB.i2cAddress = 0x0a
 TB.Init()
 
+#Motor control code
 TB.SetMotors(-0.5)
 time.sleep(3)
 TB.SetMotors(0)
