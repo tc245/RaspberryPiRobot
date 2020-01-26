8ff956f53aeab7551716294a923e2722cde84469
tc245 <34371224+tc245@users.noreply.github.com>
2020-01-26 22:14:48 +0000

    Update Thunderborg_control.py

--- robot/movement/Thunderborg_control.py
+++ robot/movement/Thunderborg_control.py
@@ -1,7 +1,14 @@
 #Thunderborg board to control robot
 
 import sys
+import time
 
 sys.path.append('/home/pi/thunderborg')
 import ThunderBorg
 
+TB = ThunderBorg.ThunderBorg()
+TB.init
+
+TB.SetMotors(0.1)
+time.sleep(2)
+TB.SetMotors(0)
