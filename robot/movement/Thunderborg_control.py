cb289a950d9d01307a13557ff20e5b3e610b0b4c
CLEMENS Tom <tclemens@ed.ac.uk>
2020-02-02 13:10:48 +0000

    minor changes

--- robot/movement/Thunderborg_control.py
+++ robot/movement/Thunderborg_control.py
@@ -120,10 +120,10 @@ while not done:
                 print("User Quit")
                 done = True
                 
-            if joystick.get_button(0):
+            elif joystick.get_button(0):
                 call(["aplay", "/home/pi/RaspberryPiRobot/robot/sound/SoundsRepository/car_horn.wav"])
                 
-            if joystick.get_button(1):
+            elif joystick.get_button(1):
                 os.chdir("/home/pi/RaspberryPiRobot/robot/photos")
                 imcount += 1
                 call(["aplay", "/home/pi/RaspberryPiRobot/robot/sound/SoundsRepository/camera_shutter.wav"])
