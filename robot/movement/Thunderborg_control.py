c5435bee21964e05e0850fd5dd13dccab08ff7a1
CLEMENS Tom <tclemens@ed.ac.uk>
2020-02-04 11:42:39 +0000

    Update Thunderborg_control.py

--- robot/movement/Thunderborg_control.py
+++ robot/movement/Thunderborg_control.py
@@ -162,20 +162,21 @@ while not done:
             elif joystick.get_button(2): #Disco mode!
                 disco = True
                 while disco:
-                    t = time.time()
-                    b = (math.sin(t * 2) + 1) / 2
-                    b = int(b * 255.0)
-                    t = round(time.time() * 1000) / 1000
-                    a = round(math.sin(t) * 90)
-                    PT.pan(int(a))
-                    PT.tilt(int(a))
-                    r, g, b = [int(x*255) for x in  colorsys.hsv_to_rgb(((t*100) % 360) / 360.0, 1.0, 1.0)]
-                    PT.set_all(r, g, b)
-                    PT.show()
-                    time.sleep(0.04)
-                    if event.type == pygame.JOYBUTTONDOWN:
-                        if joystick.get_button(11):
-                            disco = False
+                    if pygame.event.peek(JOYBUTTONDOWN):
+                        disco = False
+
+                    elif pygame.event.peek(None):
+                        t = time.time()
+                        b = (math.sin(t * 2) + 1) / 2
+                        b = int(b * 255.0)
+                        t = round(time.time() * 1000) / 1000
+                        a = round(math.sin(t) * 90)
+                        PT.pan(int(a))
+                        PT.tilt(int(a))
+                        r, g, b = [int(x*255) for x in  colorsys.hsv_to_rgb(((t*100) % 360) / 360.0, 1.0, 1.0)]
+                        PT.set_all(r, g, b)
+                        PT.show()
+                        time.sleep(0.04)
 
                 
             else:
