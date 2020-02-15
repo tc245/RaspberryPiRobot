a40e86e81306681d9a4972ee5a1b024e72deaf81
CLEMENS Tom <tclemens@ed.ac.uk>
2020-02-15 23:19:52 +0000

    Update Pygame_mixer_test.py

--- robot/sound/Pygame_mixer_test.py
+++ robot/sound/Pygame_mixer_test.py
@@ -8,10 +8,10 @@ goodbye = pygame.mixer.Sound("time2die.wav")
 horn = pygame.mixer.Sound("car_horn.wav")
 camera_shutter = pygame.mixer.Sound("camera_shutter.wav")
 
-horn.play()
+"""horn.play()
 time.sleep(3)
 camera_shutter.play()
 time.sleep(3)
-
-length_cam = camera_shutter.get_length
+"""
+length_cam = camera_shutter.get_length()
 print("length_cam")
