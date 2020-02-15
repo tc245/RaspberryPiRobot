1f2ea0e418de1f33f6c24dd3d75d0d4263afc5bc
CLEMENS Tom <tclemens@ed.ac.uk>
2020-02-15 23:07:35 +0000

    Create Pygame_mixer_test.py

---
+++ robot/sound/Pygame_mixer_test.py
@@ -0,0 +1,9 @@
+import pygame
+
+pygame.mixer.init()
+os.chdir("/home/pi/RaspberryPiRobot/robot/sound/SoundsRepository/")
+goodbye = pygame.mixer.Sound("time2die.wav")
+horn = pygame.mixer.Sound("horn.wav")
+
+horn.play()
+goodbye.play()
