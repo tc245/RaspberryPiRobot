9242c0bcdabea1b4ef85adc64b6e85e9b339e031
CLEMENS Tom <tclemens@ed.ac.uk>
2020-02-23 18:24:37 +0000

    Update Thunderborg_control.py
    Fix for the button labelling and changed pan/tilt to hold position at end of range ratehr than resettign to zero.

--- robot/movement/Thunderborg_control.py
+++ robot/movement/Thunderborg_control.py
@@ -7,6 +7,46 @@
 
 #Thunderborg board to control robot
 
+"""Button mapping:
+Axis 0 - Left stick horizontal
+Axis 1 - Left stick vertical
+Axis 2 - Right stick horizontal
+Axis 3 - Left trigger
+Axis 4 -Right stick horizontal
+Axis 5 - Right stick vertical
+Axis 6 - Right trigger
+Button 0 cross
+Button 1 circle (camera)
+Button 2 triangle (disco mode)
+Button 3 square (light)
+Button 4 Left Shoulder
+Button 5 Right Shoulder
+Button 8 Share
+Button 9 Options (quit)
+Button 10 Playstation
+Button 11 Left stick button
+Button 12 Right stick button (horn)
+
+Joy hat buttons:
+(-1,0) = Left button and move camera left
+(1,0) = Right and move camera right
+(0,-1) = Down and move camera down
+(0,1) = Up and move camera up
+
+Global values
+axisUpDown = 1          # Joystick axis to read for up / down position
+axisLeftRight = 4       # Joystick axis to read for left / right position
+buttonSlow = 4          # Joystick button number for driving slowly whilst held (L2)
+slowFactor = 0.5        # Speed to slow to when the drive slowly button is held, e.g. 0.5 would be half speed
+buttonFastTurn = 5      # Joystick button number for turning fast (R2)
+camera = 1              # Button number for camera shutter
+horn = 12               # Button number for Horn
+disco = 2               # Button number for disco mode
+light = 3               # Button to turn light on and off
+quit_button = 9         # Button to quit and shutdown robot
+interval = 0.00         # Time between updates in seconds, smaller responds faster but uses more processor time
+"""
+
 import time
 import pygame
 import gpiozero
@@ -26,7 +66,7 @@ import ThunderBorg
 call(["sudo", "mount", "-a"])
 
 #Sound test
-call(["aplay", "/home/pi/RaspberryPiRobot/robot/sound/SoundsRepository/car_horn.wav"])
+#call(["aplay", "/home/pi/RaspberryPiRobot/robot/sound/SoundsRepository/car_horn.wav"])
 
 #Indicators to confirm ok to turn motors on
 led1_pi = gpiozero.LED(26)
@@ -34,28 +74,70 @@ led2_pi = gpiozero.LED(13)
 led1_pi.off()
 led2_pi.off()
 
-#Initialise pygame
-pygame.init()
-
 #Create instance of the pantilthat class
 PT = pantilthat.PanTilt()
 #create counter variables for the pantilt
-pan = 0
-tilt = 0
+pan_to = 0
+tilt_to = 0
 #Set up the camera light
 PT.light_mode(pantilthat.WS2812)
 PT.light_type(pantilthat.GRBW)
 
 #Centre the camera
-PT.pan(pan)
-PT.tilt(tilt)
+PT.pan(pan_to)
+PT.tilt(tilt_to)
 
 #disable servo until needed to save power
 PT.idle_timeout(2)
 
+#create robot object
+#Set-up the thunderborg object
+TB = ThunderBorg.ThunderBorg()
+TB.i2cAddress = 0x0a
+TB.Init()
+
+#create camera object and set up neopixels
+camera = picamera.PiCamera()
+camera.rotation = 180
+
+# Power settings
+voltageIn = 12.0                        # Total battery voltage to the ThunderBorg
+voltageOut = 7.0                       # Maximum motor voltage
+
+# Setup the power limits
+if voltageOut > voltageIn:
+    maxPower = 1.0
+else:
+    maxPower = voltageOut / float(voltageIn)
+
+# Show battery monitoring settings
+battMin, battMax = TB.GetBatteryMonitoringLimits()
+battCurrent = TB.GetBatteryReading()
+print 'Battery monitoring settings:'
+print '    Minimum  (red)     %02.2f V' % (battMin)
+print '    Half-way (yellow)  %02.2f V' % ((battMin + battMax) / 2)
+print '    Maximum  (green)   %02.2f V' % (battMax)
+print
+print '    Current voltage    %02.2f V' % (battCurrent)
+print
+
+# Setup pygame and wait for the joystick to become available
+TB.MotorsOff()
+TB.SetLedShowBattery(False)
+TB.SetLeds(0,0,1)
+os.environ["SDL_VIDEODRIVER"] = "dummy" # Removes the need to have a GUI window
+pygame.init()
+#and the pygame mixer
+pygame.mixer.init()
+os.chdir("/home/pi/RaspberryPiRobot/robot/sound/SoundsRepository/")
+goodbye = pygame.mixer.Sound("time2die.wav")
+horn = pygame.mixer.Sound("car_horn.wav")
+camera_shutter = pygame.mixer.Sound("camera_shutter.wav")
+
 #Blinking LEDs to show controller not connected
 ready = False
 while not ready:
+    TB.SetLeds(0,0,1)
     led1_pi.on()
     led2_pi.on()
     time.sleep(0.5)
@@ -70,51 +152,39 @@ while not ready:
         led2_pi.off()
         ready = True
 
-#Create joystick object
-joystick = pygame.joystick.Joystick(0)
-joystick.init()
-
 #Light LEDs to show controller working
 led1_pi.on()
 led2_pi.on()
 
-#create robot object
-#Set-up the thunderborg object
-TB = ThunderBorg.ThunderBorg()
-TB.i2cAddress = 0x0a
-TB.Init()
+#Create joystick object
+joystick = pygame.joystick.Joystick(0)
+joystick.init()
+TB.SetLedShowBattery(True)
+ledBatteryMode = True
 
-#create camera object and set up neopixels
-camera = picamera.PiCamera()
-camera.rotation = 180
-
-#Light on flag
-light=False
-
-#Flag to exit horn loop
-horn = False
-
-# Power settings
-voltageIn = 12.0                        # Total battery voltage to the ThunderBorg
-voltageOut = 7.0                       # Maximum motor voltage
-
-# Setup the power limits
-if voltageOut > voltageIn:
-    maxPower = 1.0
-else:
-    maxPower = voltageOut / float(voltageIn)
+#Joystick settings
+#Axes
+axisUpDown = 1          # Joystick axis to read for up / down position
+axisLeftRight = 4       # Joystick axis to read for left / right position
+#buttons
+buttonSlow = 4          # Joystick button number for driving slowly whilst held (L2)
+slowFactor = 0.5        # Speed to slow to when the drive slowly button is held, e.g. 0.5 would be half speed
+buttonFastTurn = 5      # Joystick button number for turning fast (R2)
+camera_button = 1       # Button number for camera shutter
+horn_button = 12        # Button number for Horn
+disco_button = 2        # Button number for disco mode
+light_button = 3        # Button to turn light on and off
+quit_button = 9         # Button to quit and shutdown robot
+#Other settings
+interval = 0.00         # Time between updates in seconds, smaller responds faster but uses more processor time
 
 #create flag object to exit main program loop
 done = False
 
-#Constants for text to speak function call
-cmd = 'espeak '
-errors = ' 2>/dev/null' # To play back the stored .wav file and to dump the std errors to /dev/null
-#Different messages
-goodbye = "i am leaving now, robot camera signing off, goodbye"
-goodbye = goodbye.replace(' ', '_') 
-wrong_button = "sorry, this button is not mapped. try another one"
-wrong_button = wrong_button.replace(' ', '_') 
+#other flag variables
+PT.set_all(0, 0, 0, 0)
+PT.show()
+light_on = False
 
 # -------- Main Program Loop -----------
 while not done:
@@ -126,14 +196,14 @@ while not done:
     for event in pygame.event.get(): # User did something.
         if event.type == pygame.JOYBUTTONDOWN:
             
-            if joystick.get_button(9) == True: #when share pressed quit loop
+            if joystick.get_button(quit_button) == True: #when share pressed quit loop
                 print("User Quit")
                 done = True
                 
-            elif joystick.get_button(12): #Horn
+            elif joystick.get_button(horn_button): #Horn
                 call(["aplay", "/home/pi/RaspberryPiRobot/robot/sound/SoundsRepository/car_horn.wav"])
                 
-            elif joystick.get_button(1): #camera and 
+            elif joystick.get_button(camera_button): #camera and 
                 os.chdir("/home/pi/Pictures")
                 d = datetime.now() #Collect time stamp when picture taken
                 year = str(d.year)
@@ -141,10 +211,11 @@ while not done:
                 day = str(d.day)
                 hour = str(d.hour)
                 mins = str(d.minute)
+                secs = str(d.second)
                 call(["aplay", "/home/pi/RaspberryPiRobot/robot/sound/SoundsRepository/camera_shutter.wav"]) #shutter noise
-                camera.capture("{0}_{1}_{2}_{3}_{4}.jpeg".format(day, month, year, hour, mins), format="jpeg") #Image capture
+                camera.capture("{0}_{1}_{2}_{3}_{4}_{5}.jpeg".format(day, month, year, hour, mins, secs), format="jpeg") #Image capture
 
-            elif joystick.get_button(11): #Light on and off
+            elif joystick.get_button(light_button): #Light on and off
                 if light:
                     PT.set_all(0, 0, 0, 0)
                     PT.show()
@@ -154,37 +225,34 @@ while not done:
                     PT.set_all(255, 255, 255, 255)
                     PT.show()
                     light = True
-                    
-            else:
-                call([cmd+wrong_button+errors], shell=True) #Calls the Espeak TTS Engine to read aloud the Text
 
         elif event.type == pygame.JOYAXISMOTION: #Grab forward axis values
             
-            if joystick.get_axis(1) != 0:
+            if joystick.get_axis(axisUpDown) != 0:
                 
-                if joystick.get_axis(1) > 0:
+                if joystick.get_axis(axisUpDown) > 0:
                     forward = 0 - joystick.get_axis(1)
                     TB.SetMotors(forward*maxPower)
                 
-                elif joystick.get_axis(1) < 0: #and backwards
+                elif joystick.get_axis(axisUpDown) < 0: #and backwards
                     backward = 0 - (joystick.get_axis(1)) #To positive values
                     TB.SetMotors(backward*maxPower)
             
-            elif joystick.get_axis(3) !=0: #axis values for robot left
+            elif joystick.get_axis(axisLeftRight) !=0: #axis values for robot left
                 
-                if joystick.get_axis(3) > 0:
-                    leftMotorForward = joystick.get_axis(3)
-                    rightMotorReverse = (-1) + (1-joystick.get_axis(3))
+                if joystick.get_axis(axisLeftRight) > 0:
+                    leftMotorForward = joystick.get_axis(axisLeftRight)
+                    rightMotorReverse = (-1) + (1-joystick.get_axis(axisLeftRight))
                     TB.SetMotor1(leftMotorForward*maxPower)
                     TB.SetMotor2(rightMotorReverse*maxPower)                    
                 
-                elif joystick.get_axis(3) < 0: #and right
-                    leftMotorReverse = joystick.get_axis(3)
-                    rightMotorForward = 1 - (1 + joystick.get_axis(3))
+                elif joystick.get_axis(axisLeftRight) < 0: #and right
+                    leftMotorReverse = joystick.get_axis(axisLeftRight)
+                    rightMotorForward = 1 - (1 + joystick.get_axis(axisLeftRight))
                     TB.SetMotor1(leftMotorReverse*maxPower)
                     TB.SetMotor2(rightMotorForward*maxPower) 
                     
-            elif joystick.get_axis(1) == 0 or joystick.get_axis(3) == 0:
+            elif joystick.get_axis(axisUpDown) == 0 or joystick.get_axis(axisLeftRight) == 0:
                 TB.MotorsOff() #stop robot with axis values = 0
 
         elif event.type == pygame.JOYHATMOTION: #move the pan tilt servors with d-pad
@@ -192,7 +260,7 @@ while not done:
             if joystick.get_hat(0) == (1, 0):
 
                 if PT.get_pan() > 75 or PT.get_pan() < -75:
-                    pan = 0
+                    pan = pan
                     print(pan)
                     PT.pan(pan)
      
@@ -205,7 +273,7 @@ while not done:
             elif joystick.get_hat(0) == (-1, 0):
                 
                 if PT.get_pan() > 75 or PT.get_pan() < -75:
-                    pan = 0
+                    pan = pan
                     print(pan)
                     PT.pan(pan)
      
@@ -219,7 +287,7 @@ while not done:
             elif joystick.get_hat(0) == (0, 1):
                 
                 if PT.get_tilt() > 75 or PT.get_tilt() < -75:
-                    tilt = 0
+                    tilt = tilt
                     print(tilt)
                     PT.tilt(tilt)
      
@@ -232,7 +300,7 @@ while not done:
             elif joystick.get_hat(0) == (0, -1):
                 
                 if PT.get_tilt() > 75 or PT.get_tilt() < -75:
-                    tilt = 0
+                    tilt = tilt
                     print(tilt)
                     PT.tilt(tilt)
      
