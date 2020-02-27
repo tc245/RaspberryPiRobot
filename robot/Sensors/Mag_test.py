c908dd4a68f3c2d95393caeddd9f32c1d1ee6cec
CLEMENS Tom <tclemens@ed.ac.uk>
2020-02-27 17:49:05 +0000

    Update Mag_test.py

--- robot/Sensors/Mag_test.py
+++ robot/Sensors/Mag_test.py
@@ -6,14 +6,14 @@ import csv
 
 lsm = LSM303D(0x1d) # Change to 0x1e if you have soldered the address jumper
 
-with open('gauss.csv', 'w') as file:
+with open('gauss2.csv', 'w') as file:
     writer = csv.writer(file)
-    writer.writerow(["X", "Y", "Z"])
+    writer.writerow(["X", "Z", "Y"])
 
     for _ in range(150):
         xyz = list(lsm.magnetometer())
         print(("{:+06.2f} : {:+06.2f} : {:+06.2f}").format(*xyz))
-        with open('gauss.csv', 'a') as file:
+        with open('gauss2.csv', 'a') as file:
             writer = csv.writer(file)
             writer.writerow(xyz[0:3])
         time.sleep(0.2)
