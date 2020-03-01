cf70deffe7d2ec79b8d34a1a36465768f555081f
CLEMENS Tom <tclemens@ed.ac.uk>
2020-03-01 14:47:30 +0000

    Update NorthZeroValues.py

--- robot/Sensors/NorthZeroValues.py
+++ robot/Sensors/NorthZeroValues.py
@@ -12,7 +12,7 @@ Y = 2  # Change to 1 if you have the breakout flat
 # Precalculated offsets from calibration exercise
 offsets = [0.08085445, 0, 0.08645489]
 
-input("Set a zero (North) point, then turn your breakout to that point and press a key...")
+input("Set a zero (North) point, then turn your breakout to that point and press a key...\")
 
 # Get the magnetometer's values
 mag = list(lsm.magnetometer())
