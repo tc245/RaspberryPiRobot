289c4f062c7e187ad344747c263e3bd655b2cc8e
CLEMENS Tom <tclemens@ed.ac.uk>
2020-06-21 18:22:03 +0100

    Create HuskLensTest.py

---
+++ robot/HuskyLens/HuskLensTest.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 21 18:18:45 2020
+
+@author: tclemens
+"""
+
+sys.path.append('/home/pi/HUSKYLENSPython')
+from huskylensPythonLibrary import HuskyLensLibrary
+
+husky= HuskyLensLibrary("I2C","",address=0x32)
+
+print(husky.command_request_knock()) 
