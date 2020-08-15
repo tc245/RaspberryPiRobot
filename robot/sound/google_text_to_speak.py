7fb00822f175ba745668b9dd0018bfc90b0726d0
CLEMENS Tom <tclemens@ed.ac.uk>
2020-08-15 17:09:50 +0100

    Update google_text_to_speak.py

--- robot/sound/google_text_to_speak.py
+++ robot/sound/google_text_to_speak.py
@@ -27,7 +27,7 @@ synthesis_input = texttospeech.SynthesisInput(text=text)
 # Build the voice request, select the language code ("en-US") and the ssml
 # voice gender ("neutral")
 voice = texttospeech.VoiceSelectionParams(
-    language_code="en-US", ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
+    language_code="en-GB", ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
 )
 
 # Select the type of audio file you want returned
