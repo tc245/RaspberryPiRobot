3f60610aec46be6dacfca839b945c1e3b861b729
CLEMENS Tom <tclemens@ed.ac.uk>
2020-08-22 15:46:53 +0100

    Update TestServer.py

--- robot/ThunderBorgCode/TestServer.py
+++ robot/ThunderBorgCode/TestServer.py
@@ -25,11 +25,12 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
         self.request.sendall(self.data.upper())
 
 if __name__ == "__main__":
-    HOST, PORT = "localhost", 9999
+    HOST, PORT = "", 9999
 
     # Create the server, binding to localhost on port 9999
     with socketserver.TCPServer((HOST, PORT), MyTCPHandler) as server:
         # Activate the server; this will keep running until you
         # interrupt the program with Ctrl-C
-        server.serve_forever()
         print("Server started")
+        server.serve_forever()
+        
